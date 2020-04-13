7a94dc3bbbfeeddeb8672c554c70d08a1d8a6b87
Oleg Nenashev <o.v.nenashev@gmail.com>
2020-04-13 22:00:10 +0200

    Stop building Windows images with shortnames

--- make.ps1
+++ make.ps1
@@ -23,11 +23,11 @@ if(![String]::IsNullOrWhiteSpace($env:DOCKERHUB_ORGANISATION)) {
 $builds = @{
     'jdk8' = @{
         'Folder' = '8\windowsservercore-1809';
-        'Tags' = @( "latest", "windowsservercore-1809", "jdk8", "windowsservercore-1809-jdk8" );
+        'Tags' = @( "windowsservercore-1809", "windowsservercore-1809-jdk8" );
     };
     'jdk11' = @{
         'Folder' = '11\windowsservercore-1809';
-        'Tags' = @( "windowsservercore-1809-jdk11", "jdk11" );
+        'Tags' = @( "windowsservercore-1809-jdk11" );
     };
     'nanoserver' = @{
         'Folder' = '8\nanoserver-1809';
